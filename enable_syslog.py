eb1be887303ed726bb39906cc55be6546840d18f
erofeev <erofeev@smartintel.ru>
2020-11-23 14:28:06 +0300

    Добавлен модуль включения syslog-a для D-Linka-a и Eltex-a.

--- enable_syslog.py
+++ enable_syslog.py
@@ -2,7 +2,7 @@ import json
 from loguru import logger
 import pynetbox
 import netmiko
-
+import logging
 with open("config.json") as json_conf_file:
     conf = json.load(json_conf_file)
 
@@ -16,101 +16,102 @@ if not nb_conf:
 
 nb = pynetbox.api(nb_conf['url'], nb_conf['token'], threading=True)
 
+logging.basicConfig(filename='test.log', level=logging.DEBUG)
+# logger = logging.getLogger("netmiko")
 
-# commands = {
-    # 'eltex':
-    #     ['sh ip ssh'],
-#     'd-link':
-#         [
-#         'enable ssh',
-#         ],
-# }
+commands = {
+    'eltex':
+        ['show syslog-servers'],
+    'd-link':
+        [
+        'create syslog host 1 ipaddress 10.100.3.130 udp_port 514 severity warning facility local7 state enable',
+        'config syslog host 1 ipaddress 10.100.3.130 udp_port 514 severity warning',
+        'enable syslog',
+        'show syslog host',
+        'enable clipaging',
+        ],
+}
 
 configs = {
     'eltex':
         [
             'logging buffered',
             'logging buffered 400',
-            'logging host 10.100.3.130 port 1514 severity debugging',
-
+            'logging host 10.100.3.130 port 514 severity debugging',
         ]
 }
 
 fail_to_connect = []
-error = []
 finish_result = []
 
-
-# list_ip_for_enable_syslog = nb.dcim.devices.filter(region='kb', role='access-switch')
-
-
-
-
-# for device in list_ip_for_enable_syslog:
+# получаем из нетбокса список коммутаторов для включения syslog-а
+# list_for_enable_syslog = nb.dcim.devices.filter(tag='test-syslog', role='access-switch')
+list_for_enable_syslog = nb.dcim.devices.filter(region='parkovskaya-3', role='access-switch', status='active')
 
 
-_params = {
-    # 'device_type': device['netmiko_driver'],
-    'device_type': 'eltex_telnet',
-    'username': dev_conf['username'],
-    'password': dev_conf['password'],
-    'ip': '10.100.0.207'
-    # 'ip': device['ip'],
-}
+#запускаем цикл по коммутаторам
+for device in list_for_enable_syslog:
+    _driver = device.device_type.custom_fields['netmiko_driver']
+    _vendor = device.device_type.manufacturer.slug
+    _ip = device.primary_ip.address.split('/')[0] # отрезаем префикс от ip
+    _params = {
+        'device_type': _driver, # имя драйвера для netmiko
+        'username': dev_conf['username'],
+        'password': dev_conf['password'],
+        'ip': _ip,
+    }
 
-_device_result = {}
-try:
-
-    with netmiko.ConnectHandler(**_params) as ssh:
-        # посылаем конфиг Элтекса, если он есть
-        if configs.get('eltex'):
-
-            out = ssh.send_config_set(configs.get('eltex'), cmd_verify=False)
-            # logger.info(f'Выполняю configs Элтекса: {out}')
+    _device_result = {}
 
 
-        # if commands.get(device['vendor']):
-        #     _vendor = device['vendor']
+    try:
+        with netmiko.ConnectHandler(**_params) as ssh:
 
-            # _device_result['vendor'] = _vendor
-
-
-            # for command in commands.get(device['vendor']):
-            #     _ip = device['ip']
-            #     _device_result['ip'] = _ip
-
-            #     if 'config admin local_enable' in command:
-            #         out = ssh.send_command_timing(command, use_textfsm=True)
-            #         while isinstance(out, str) and out.startswith(':', -1):
-            #             out = ssh.send_command_timing("\n")
-            #             out = netmiko.utilities.get_structured_data(out, platform=_params['device_type'], command=command)
-            #     else:
-            #         out = ssh.send_command(command, use_textfsm=True)
-            #     logger.info(f'Применяю команду: {command} на {_vendor} IP: {_ip} {out}')
-
-            #     if not isinstance(out, list):
-            #         error.append({_ip, out})
-
-                    # continue
-                    # raise ValueError("Error in result", out, device, command)
+            # посылаем конфиг Элтекса, если он есть
+            if cfg := configs.get(_vendor):
+                out = ssh.send_config_set(cfg, cmd_verify=False)
+                logger.info(f'Выполняю configs Элтекса:{_ip} {out}')
+                _device_result['ip'] = _ip
+                _device_result['vendor'] = _vendor
 
 
-        ssh.save_config()
+            if commands.get(_vendor):
+                _device_result['vendor'] = _vendor
 
 
-except (TimeoutError, ConnectionResetError, netmiko.ssh_exception.NetMikoTimeoutException) as ex:
-    _device_result['ip'] = _params['ip']
-    # _device_result['vendor'] = device['vendor']
-    _device_result['error'] = f'{ex.args} - {ex.__class__.__name__}'
+                for command in commands.get(_vendor):
+                    _device_result['ip'] = _ip
+                    out = ssh.send_command(command, use_textfsm=True)
+                    logger.info(f'Применяю команду: {command} на {_vendor} IP: {_ip} {out}')
 
-    # fail_to_connect.append({device['ip']: _device_result['error']})
+                    if 'show syslog-servers' in command:
+                        _device_result['status'] = out[0]['status']
 
-    # logger.info(f'Невозможно соединиться{fail_to_connect[-1]}')
+                    if 'enable syslog' in command:
+                        _device_result['enable_syslog'] = out[0]['enable']
 
-# finish_result.append(_device_result)
+                    if 'show syslog host' in command:
+                        _device_result['ip_syslog_sever'] = out[0]['ip']
 
-logger.info(f'{_device_result}')
-# logger.info(f'{error}')
 
-# with open('output/enable_ssh.json', 'w', encoding='utf-8-sig') as json_file:
-#     json.dump(finish_result, json_file, indent=4, sort_keys=True)
+            ssh.save_config()
+
+        finish_result.append(_device_result)
+
+    except (TimeoutError, ConnectionResetError, netmiko.ssh_exception.NetMikoTimeoutException) as ex:
+        _device_result['ip'] = _ip
+        _device_result['vendor'] = _vendor
+        _device_result['error'] = f'{ex.args} - {ex.__class__.__name__}'
+
+        fail_to_connect.append(_device_result)
+        logger.info(f'Невозможно соединиться{fail_to_connect[-1]}')
+
+
+    logger.info(f'{_device_result}')
+logger.info(f'{finish_result}')
+
+with open('output/success_enable_syslog.json', 'w', encoding='utf-8-sig') as json_file:
+    json.dump (finish_result, json_file, indent=4, sort_keys=True)
+
+with open('output/errors_enable_syslog.json', 'w', encoding='utf-8-sig') as json2_file:
+    json.dump(fail_to_connect, json2_file, indent=4, sort_keys=True)
